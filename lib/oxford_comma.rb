228ec623231a0e1e663b0bc99641e32193ab4dd3
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 06:15:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,21 @@ require "pry"
  def oxford_comma(array)
 
 if array.length==2
-  array.join(" and ")
+  
+ return  array.join(" and ")
 elsif array.length > 2
-  binding.pry
+ # binding.pry
+array[-1].insert(0, "and ")
 end
+return array.join(", ")
+
+
+
+
 end
-exitarray[-1].insert(0, "hello ")
 
 
 
-array_instance.insert(index,object)
+
+
 
