98e850cc9990e098a838898cb7290e1223c6edce
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 05:41:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,15 @@ require "pry"
 
  def oxford_comma(array)
 
+if array.length==2
+  array.join(" and ")
+elsif array.length > 2
   binding.pry
 end
 end
+exitarray[-1].insert(0, "hello ")
+
+
+
+array_instance.insert(index,object)
+
