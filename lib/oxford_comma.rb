fc2f545c7a5aa38e3a003a5de3d7bc4477e9efd6
Chris Beyer <beyercorpuz@gmail.com>
2020-02-01 06:12:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,12 +3,16 @@ require "pry"
  def oxford_comma(array)
 
 if array.length==2
+  return 
   array.join(" and ")
 elsif array.length > 2
  # binding.pry
 array[-1].insert(0, "and ")
 end
 return array.join(",")
+
+
+
 end
 
 
